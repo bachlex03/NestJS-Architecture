0ebd4c46141290f0a9b392321cced10fcda48cf0
lxbachit03 <lxbachit03.working@gmail.com>
2024-06-15 18:12:00 +0700

    packages: helmet, compression and enable cors with specific FE url

--- src/config/env.type.ts
+++ src/config/env.type.ts
@@ -1,6 +1,9 @@
 export type EnvironmentFields = {
   PORT: number;
+
   DB_NAME: string;
   DB_USERNAME: string;
   DB_PASSWORD: string;
+
+  FRONTEND_URL: string;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Security middleware
+  // prevent common security vulnerabilities by setting HTTP headers appropriately
+  app.use(helmet());
+
+  // Compression reduce the size of the response body and increase the speed of a web app
+  app.use(compression());
+
   const configService = app.get(ConfigService);
+
+  app.enableCors({ origin: configService.get('FRONTEND_URL') });
 
   const port = configService.get('PORT') || 3000;
 
